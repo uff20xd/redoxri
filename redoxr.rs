7943d3261a4a010cee51b527cea5ecd7deab0ed8
uff20xd <uff20xd@proton.me>
2025-08-28 18:03:54 +0200

    k

---
+++ redoxr.rs
@@ -0,0 +1,33 @@
+/// Welcome to Redoxr
+
+use std::{
+    process::{
+        Command,
+    },
+};
+
+pub type Cmd = Command;
+
+pub struct Mcule {
+    name: String,
+    outpath: String,
+    inputs: &[],
+    command: Cmd,
+}
+
+pub struct CMcule {
+
+}
+
+pub enum RustCrateType {
+    ProcMacro,
+    Bin,
+    Lib,
+    Rlib,
+}
+
+pub struct RustMcule {
+    crate_type: RustCrateType,
+    outpath: String,
+
+}
