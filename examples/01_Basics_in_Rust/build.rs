f138884eb977ef73dff545561aee07c80f0945d6
uff20xd <uff20xd@proton.me>
2025-12-25 19:54:25 +0100

    k

--- examples/01_Basics_in_Rust/build.rs
+++ examples/01_Basics_in_Rust/build.rs
@@ -4,9 +4,14 @@ use redoxri::*;
 fn main() -> () {
     let _redoxri = Redoxri::new(&[""]);
 
+    let out = Mcule::new("out", "out")
+        .with(&["out".into()])
+        .add_step(&["mkdir", "$out"])
+        .compile();
+
     let main_src = Mcule::new("main_src", "src/main.rs");
 
-    let _main = Mcule::new("fibonacci", "fibonacci")
+    let _main = Mcule::new("fibonacci", "out/fibonacci")
         .with(&[main_src.clone()])
         .add_step(&["rustc", &main_src.outpath, "-o", "$out"])
         .compile()
