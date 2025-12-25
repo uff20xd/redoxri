993075c9f9db080e6ba48feda7707a370ecdae5c
uff20xd <uff20xd@proton.me>
2025-12-25 20:07:20 +0100

    k

--- examples/01_Basics_in_Rust/src/main.rs
+++ examples/01_Basics_in_Rust/src/main.rs
@@ -1,8 +1,19 @@
 fn main() {
-
+    let n = 0;
+    println!("{n}! is {}", fibonacci(n))
 }
 
-fn fibonacci() -> u128 {
-    0
+fn fibonacci(n: u128) -> u128 {
+    let mut a = 1;
+    let mut b = 1;
+    let mut c = 2;
+
+    for i in 0..n {
+        a = b;
+        b = c;
+        c = a + b;
+    }
+
+    a
 }
 
